5c41653cff86da8e8e6ffb39a92f89950ce33a9b
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-03-24 15:58:08 +0600

    add database connection and add get method

--- index.js
+++ index.js
@@ -8,31 +8,56 @@ const port = process.env.PORT || 5000;
 // middleware 
 app.use(cors({
     origin: [
-      "https://sushil-portfolio.surge.sh",
-      "http://localhost:5173",
-      "http://localhost:5174",
+        "https://sushil-portfolio.surge.sh",
+        "http://localhost:5173",
+        "http://localhost:5174",
     ],
     credentials: true,
     optionsSuccessStatus: 200,
-  }));
-  app.use(express.json());
+}));
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ruakr2a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        const tasksCollection = client.db('taskTracker').collection('tasks');
+        // await client.connect();
+        app.get('/tasks', async (req, res) => {
+            try {
+                const result = await tasksCollection.find().toArray();
+                res.send(result)
+            }
+            catch (error) {
+                console.log(error);
+            }
+        })
+      
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-  app.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.send("Server is Running Now.....")
-  })
-  app.listen(port, () => {
+})
+app.listen(port, () => {
     console.log(`Server Running on port: ${port}`);
-  })
+})
